bf2bf0b2c93fc9a08c6b335fb8eb313c454cc8f9
Sabry Razick <sabryr@itf-lifeportal-tools-d01.hpc.uio.no>
2016-12-06 16:49:01 +0100

    Chnaged to copy to current directory

--- pre_slurm_RPM_install.sh
+++ pre_slurm_RPM_install.sh
@@ -11,7 +11,7 @@ echo "for i  in $(VER=0.5.10-1;ls | grep $VER); do printf \"/export/rocks/instal
 
 echo "Starting copying RPMS from nielshenrik.abel.uio.no" 
 scp -p nielshenrik.abel.uio.no:"/export/rocks/install/contrib/6.2/x86_64/RPMS/slurm-15.08.8-1.el6.x86_64.rpm /export/rocks/install/contrib/6.2/x86_64/RPMS/slurm-devel-15.08.8-1.el6.x86_64.rpm /export/rocks/install/contrib/6.2/x86_64/RPMS/slurm-munge-15.08.8-1.el6.x86_64.rpm /export/rocks/install/contrib/6.2/x86_64/RPMS/slurm-pam_slurm-15.08.8-1.el6.x86_64.rpm /export/rocks/install/contrib/6.2/x86_64/RPMS/slurm-perlapi-15.08.8-1.el6.x86_64.rpm /export/rocks/install/contrib/6.2/x86_64/RPMS/slurm-plugins-15.08.8-1.el6.x86_64.rpm" .
-scp -p nielshenrik.abel.uio.no:"/export/rocks/install/contrib/6.2/x86_64/RPMS/munge-0.5.10-1.el6.x86_64.rpm /export/rocks/install/contrib/6.2/x86_64/RPMS/munge-devel-0.5.10-1.el6.x86_64.rpm /export/rocks/install/contrib/6.2/x86_64/RPMS/munge-libs-0.5.10-1.el6.x86_64.rpm"
+scp -p nielshenrik.abel.uio.no:"/export/rocks/install/contrib/6.2/x86_64/RPMS/munge-0.5.10-1.el6.x86_64.rpm /export/rocks/install/contrib/6.2/x86_64/RPMS/munge-devel-0.5.10-1.el6.x86_64.rpm /export/rocks/install/contrib/6.2/x86_64/RPMS/munge-libs-0.5.10-1.el6.x86_64.rpm" .
 
 
 echo "Install the rpms using sudo rpm -iav *rpm"
