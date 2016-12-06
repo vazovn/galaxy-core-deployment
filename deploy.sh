b8ce5b56ad7baafcf9f7208d30f7f2eeed31bff7
tt <trond.thorbjornsen@usit.uio.no>
2016-12-06 10:33:44 +0100

    Added optional mounting of work abel

--- deploy.sh
+++ deploy.sh
@@ -7,18 +7,22 @@ MYDIR="$(dirname "$(realpath "$0")")"
 echo ${MYDIR}
 
 # db
-read -p "Database url on the form: postgresql://username:password@localhost/mydatabase (leave empty for local sqlite)" dburl
+read -p "Database url on the form: postgresql://username:password@localhost/mydatabase (leave empty for local sqlite)\n> " dburl
 
 # setup
 if [ "$1" == "production" ]; then
     production=y
 else
-    read -p "Is this a production server? [yN]" production
+    read -p "Is this a production server? [yN] " production
 fi
 
-# sudo sh -c 'echo galaxy:x:182649:70731:galaxy:/home/galaxy:/bin/bash >> /etc/passwd'
-# sudo mkdir /home/galaxy
-# sudo chown galaxy:galaxy /home/galaxy/
-# sudo yum install git
+read -p "Mount /work on abel (host needs to be added to nfs on abel first)? [yN] " workonabel
+
+sudo sh -c 'echo galaxy:x:182649:70731:galaxy:/home/galaxy:/bin/bash >> /etc/passwd'
+sudo mkdir /home/galaxy
+sudo chown galaxy:galaxy /home/galaxy/
+sudo yum install git
+
+sudo sed -i.orig-$(date "+%y-%m-%d-%H%M") -e "\$a# For /work/projects/galaxy\nadmin.abel.uio.no:/work    /work    nfs4    defaults    0 0" /etc/fstab
 
 sudo -u galaxy -H sh -c "${MYDIR}/as_galaxy_user.sh ${production} ${dburl}"
