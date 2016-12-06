123d16a39eac01a01548fb27f45c18fa4ed8fdf6
Nikolay Aleksandrov Vazov <nikolaiv@markov.uio.no>
2016-12-06 13:23:14 +0100

    Script for configuring galaxy.ini

--- as_galaxy_user.sh
+++ as_galaxy_user.sh
@@ -28,7 +28,7 @@ function sed_replace {
 
 
 # galaxy ini:
-echo "check if galaxy.ini exists"
+echo "check if galaxy.ini exists"8(
 cd ${GALAXYTREE}/config
 if [ ! -f galaxy.ini ]; then
     cp galaxy.ini.sample galaxy.ini
@@ -39,8 +39,7 @@ fi
 echo "production?"
 echo ${production}
 if [ "${production}" == "y" ]; then
-        sed_replace '^#debug = False' 'debug =  False/' galaxy.ini
-        sed_replace '^use_interactive = True' 'use_interactive = False' galaxy.ini
+        sed_replace '^use_interactive = .*' 'use_interactive = False' galaxy.ini
         echo "replaced debug and use_interactive from galaxy.ini"
 fi
 
@@ -50,3 +49,75 @@ if [ ! -z ${dburl} ]; then
     sed_replace '#database_connection.*' "database_connection = ${dburl}" galaxy.ini
     echo "replaced dburl from galaxy.ini"
 fi
+
+# Fra Nikolay
+sed_replace '^#admin_users.*' 'admin_users = ' galaxy.ini
+
+## General
+sed_replace '^#port =.*' 'port = 8080' galaxy.ini
+sed_replace '^#host =.*' 'host = 127.0.0.1' galaxy.ini
+
+## DB
+sed_replace '^#database_engine_option_pool_size =.*' 'database_engine_option_pool_size = 5' galaxy.ini
+sed_replace '^#database_engine_option_max_overflow =.*' 'database_engine_option_max_overflow = 10' galaxy.ini
+sed_replace '^#database_engine_option_server_side_cursors = False' 'database_engine_option_server_side_cursors = True' galaxy.ini
+
+## PATHS / DIRS
+sed_replace '^#new_file_path =.*' 'new_file_path = ${GALAXY_NEW_FILEPATH}' galaxy.ini
+sed_replace '^#file_path =.*' 'file_path =${GALAXY_FILEPATH} ' galaxy.ini
+sed_replace '^#job_working_directory =.*' 'job_working_directory =  ${GALAXY_JOB_WORKING_DIRECTORY}' galaxy.ini
+sed_replace '^#cluster_files_directory =.*' 'cluster_files_directory = ${GALAXY_CLUSTER_FILES_DIRECTORY} ' galaxy.ini
+sed_replace '^#collect_outputs_from =.*' 'collect_outputs_from = ,job_working_directory ' galaxy.ini
+
+## CONFS
+sed_replace '^#tool_config_file =.*' 'tool_config_file = config/tool_conf.xml' galaxy.ini
+sed_replace '^#integrated_tool_panel_config.*' 'integrated_tool_panel_config = integrated_tool_panel.xml' galaxy.ini
+sed_replace '^#tool_data_table_config_path = config/tool_data_table_conf.xml' 'tool_data_table_config_path = config/tool_data_table_conf.xml' galaxy.ini
+sed_replace '^#tool_data_path = tool-data' 'tool_data_path = tool-data' galaxy.ini
+
+
+## SMTP / EMAILS
+sed_replace '^#smtp_server =.*' ' smtp_server = smtp.uio.no' galaxy.ini
+sed_replace '^#error_email_to =.*' ' error_email_to = lifeportal-help@usit.uio.no' galaxy.ini
+sed_replace '^#blacklist_file = config/disposable_email_blacklist.conf ' 'blacklist_file = config/disposable_email_blacklist.conf ' galaxy.ini
+
+## BRAND
+sed_replace '^#brand = None' 'brand = ${GALAXY_BRAND}' galaxy.ini
+
+## STATIC CONTENT
+sed_replace '^#static_enabled = True' 'static_enabled = True' galaxy.ini
+sed_replace '^#static_cache_time = 360' 'static_cache_time = 360' galaxy.ini
+sed_replace '^#static_dir = static/' 'static_dir = static/' galaxy.ini
+sed_replace '^#static_images_dir = static/images' 'static_images_dir = static/images' galaxy.ini
+sed_replace '^#static_favicon_dir = static/favicon.ico' 'static_favicon_dir = static/favicon.ico' galaxy.ini
+sed_replace '^#static_scripts_dir = static/scripts/' 'static_scripts_dir = static/scripts/' galaxy.ini
+sed_replace '^#static_style_dir = static/june_2007_style/blue' 'static_style_dir = static/june_2007_style/blue' galaxy.ini
+sed_replace '^#static_robots_txt = static/robots.txt' 'static_robots_txt = static/robots.txt' galaxy.ini
+
+## DEBUG
+sed_replace '^#debug = False' 'debug =  True' galaxy.ini
+sed_replace '^#sanitize_all_html = True' 'sanitize_all_html = True' galaxy.ini
+
+## DATA LIBRARIES
+sed_replace '^#library_import_dir = None' 'library_import_dir = database/admin_upload' galaxy.ini
+sed_replace '^#user_library_import_dir = None' 'user_library_import_dir = database/user_upload' galaxy.ini
+
+## USERS / SECURITY
+sed_replace '^#use_remote_user = False' 'use_remote_user = True' galaxy.ini
+sed_replace '^#remote_user_logout_href = None' 'remote_user_logout_href = https://${GALAXY_PUBLIC_HOSTNAME}/callback?logout=http://${GALAXY_PUBLIC_HOSTNAME}/' galaxy.ini
+sed_replace '^#normalize_remote_user_email = False ' 'normalize_remote_user_email = True ' galaxy.ini
+sed_replace '^#admin_users = None' 'admin_users = ${GALAXY_ADMIN_USERS}' galaxy.ini
+sed_replace '^#require_login = False' 'require_login = True' galaxy.ini
+sed_replace '^#allow_user_creation = True' 'allow_user_creation = False' galaxy.ini
+sed_replace '^#allow_user_deletion = False' 'allow_user_deletion = True' galaxy.ini
+sed_replace '^#allow_user_impersonation = False' 'allow_user_impersonation = True' galaxy.ini
+sed_replace '^#allow_user_dataset_purge = True' 'allow_user_dataset_purge = True' galaxy.ini
+sed_replace '^#new_user_dataset_access_role_default_private = False' 'new_user_dataset_access_role_default_private = False ' galaxy.ini
+sed_replace '^#expose_dataset_path = False ' 'expose_dataset_path = True' galaxy.ini
+
+## JOBS
+sed_replace '^#job_config_file = config/job_conf.xml ' 'job_config_file = config/job_conf.xml' galaxy.ini
+sed_replace '^#enable_job_recovery = True' 'enable_job_recovery = True' galaxy.ini
+sed_replace '^#cleanup_job = .*' 'cleanup_job = never' galaxy.ini
+sed_replace '^#job_resource_params_file = config/job_resource_params_conf.xml' 'job_resource_params_file = config/job_resource_params_conf.xml' galaxy.ini
+
--- settings.sh
+++ settings.sh
@@ -6,3 +6,11 @@ GALAXYUSERPID=182649
 GALAXYUSERGID=70731
 GALAXYUSERHOME=/home/galaxy
 GALAXYTREE=/home/galaxy/galaxy
+
+GALAXY_FILEPATH=/work/projects/galaxy/data/database_galaxy_prod01/files
+GALAXY_NEW_FILEPATH=/work/projects/galaxy/data/database_galaxy_prod01/tmp
+GALAXY_JOB_WORKING_DIRECTORY=/work/projects/galaxy/data/database_galaxy_prod01/job_working_directory
+GALAXY_CLUSTER_FILES_DIRECTORY=/work/projects/galaxy/data/database_galaxy_prod01/slurm
+GALAXY_BRAND=Lifeportal
+GALAXY_PUBLIC_HOSTNAME=lifeportal.uio.no
+GALAXY_ADMIN_USERS=n.a.vazov@usit.uio.no,sabry.razick@usit.uio.no
