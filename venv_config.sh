2ade8cf2efebd1efa33614c9172d8d8d22e8587f
Nikolay Aleksandrov Vazov <nikolaiv@itf-lifeportal-core-d01.hpc.uio.no>
2017-01-05 15:29:13 +0100

    Modify post configuration script

--- venv_config.sh
+++ venv_config.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
+
 # Modify $PYTHONPATH in .venv
-echo 'export GALAXY_LIB=/home/galaxy/galaxy/lib' >> /home/galaxy/galaxy/.venv/bin/activate
-echo 'export PYTHONPATH=$GALAXY_LIB:/home/galaxy/galaxy/lib/usit/python' >> /home/galaxy/galaxy/.venv/bin/activate
+sudo -u galaxy -H sh -c "export GALAXY_LIB=/home/galaxy/galaxy/lib >> /home/galaxy/galaxy/.venv/bin/activate"
+sudo -u galaxy -H sh -c "export PYTHONPATH=$GALAXY_LIB:/home/galaxy/galaxy/lib/usit/python >> /home/galaxy/galaxy/.venv/bin/activate"
 echo "PYTHONPATH SET IN .venv/bin/activate " $PYTHONPATH 
