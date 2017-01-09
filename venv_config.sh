5c1e3d825b824d41fd8f0debc2ae1a0e986a2540
Nikolay Aleksandrov Vazov <nikolaiv@itf-lifeportal-core-d01.hpc.uio.no>
2017-01-09 13:16:29 +0100

    Typo Fix adding PYTHONPATH to .venv/bin/activate

--- venv_config.sh
+++ venv_config.sh
@@ -3,4 +3,4 @@
 
 # Modify $PYTHONPATH in .venv
 sudo -u galaxy -H sh -c 'echo export GALAXY_LIB=/home/galaxy/galaxy/lib >> /home/galaxy/galaxy/.venv/bin/activate'
-sudo -u galaxy -H sh -c 'echo export export PYTHONPATH=$GALAXY_LIB:/home/galaxy/galaxy/lib/usit/python >> /home/galaxy/galaxy/.venv/bin/activate'
+sudo -u galaxy -H sh -c 'echo export PYTHONPATH=$GALAXY_LIB:/home/galaxy/galaxy/lib/usit/python >> /home/galaxy/galaxy/.venv/bin/activate'
