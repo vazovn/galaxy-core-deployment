c796f704b5703ac1a87b3b4dc54caa5fc85893bd
Nikolay Aleksandrov Vazov <nikolaiv@itf-lifeportal-core-d01.hpc.uio.no>
2017-01-09 13:15:35 +0100

    Fix adding PYTHONPATH to .venv/bin/activate

--- venv_config.sh
+++ venv_config.sh
@@ -2,6 +2,5 @@
 
 
 # Modify $PYTHONPATH in .venv
-sudo -u galaxy -H sh -c "export GALAXY_LIB=/home/galaxy/galaxy/lib >> /home/galaxy/galaxy/.venv/bin/activate"
-sudo -u galaxy -H sh -c "export PYTHONPATH=$GALAXY_LIB:/home/galaxy/galaxy/lib/usit/python >> /home/galaxy/galaxy/.venv/bin/activate"
-sudo -u galaxy -H sh -c "echo \"PYTHONPATH SET IN .venv/bin/activate \" $PYTHONPATH  >> /home/galaxy/galaxy/.venv/bin/activate"
+sudo -u galaxy -H sh -c 'echo export GALAXY_LIB=/home/galaxy/galaxy/lib >> /home/galaxy/galaxy/.venv/bin/activate'
+sudo -u galaxy -H sh -c 'echo export export PYTHONPATH=$GALAXY_LIB:/home/galaxy/galaxy/lib/usit/python >> /home/galaxy/galaxy/.venv/bin/activate'
