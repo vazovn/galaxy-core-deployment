aebe3e3e411d9434428396d0db436c42058dea69
Nikolay Aleksandrov Vazov <nikolaiv@itf-lifeportal-core-d01.hpc.uio.no>
2017-02-09 13:37:15 +0100

    Comment about the way we install wkhtmltopdf, not YUM but wget

--- deploy-pdf-modules.sh
+++ deploy-pdf-modules.sh
@@ -6,6 +6,9 @@ source settings.sh
 source ${GALAXYTREE}/.venv/bin/activate
 
 ## install wkhtmltopdf
+## The wkhtmltopdf version from YUM requires a running X-server.
+## We have to use the headless version which is linux-precompiled and available through the site below. 
+
 cd ${GALAXYTREE}/.venv/bin
 wget http://download.gna.org/wkhtmltopdf/0.12/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
 tar xvf wkhtmltox-0.12.4_linux-generic-amd64.tar.xz 
