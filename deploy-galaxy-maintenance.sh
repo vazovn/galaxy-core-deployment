78f28bce12b11c9b3e282437c35631227353ccb4
Nikolay Aleksandrov Vazov <nikolaiv@itf-lifeportal-core-d01.hpc.uio.no>
2017-02-13 11:37:44 +0100

    Fixed wrong paths for run scripts

--- deploy-galaxy-maintenance.sh
+++ deploy-galaxy-maintenance.sh
@@ -26,8 +26,15 @@ fi
 
 # e.g. /home/galaxy/galaxy
 sed -i "s#GALAXYTREE=.*#GALAXYTREE=$GALAXYTREE#"  galaxy-maintenance/maintenance_local_env.sh
+
 # e.g.  /home/galaxy
 sed -i  "s#GALAXYUSERHOME=.*#GALAXYUSERHOME=$GALAXYUSERHOME#" galaxy-maintenance/maintenance_local_env.sh
+
+sed -i  "s#GALAXYUSERHOMEPATH#$GALAXYUSERHOME#" galaxy-maintenance/scripts/galaxy_emails_management/run_get_galaxy_user_emails.sh
+sed -i  "s#GALAXYUSERHOMEPATH#$GALAXYUSERHOME#" galaxy-maintenance/scripts/lifeportal_usage_report/run_lifeportal_usage_report.sh
+sed -i  "s#GALAXYUSERHOMEPATH#$GALAXYUSERHOME#" galaxy-maintenance/scripts/manipulate_project_allocations/run_manipulate_allocations_end_date.sh
+sed -i  "s#GALAXYUSERHOMEPATH#$GALAXYUSERHOME#" galaxy-maintenance/scripts/mas_projects_maintenance/run_mas_projects_management.sh
+sed -i  "s#GALAXYUSERHOMEPATH#$GALAXYUSERHOME#" galaxy-maintenance/scripts/rogue_users/run_check_rogue_users.sh
 
 echo "Galaxy maintenance kit installed in galaxy-maintenance. Do not forget to set the cron jobs for:"
 echo "galaxy email management and mas_projects_maintenance as galaxy"
