f78243b5a9cd05e260d280ca550b21ace5a961c8
Nikolay Aleksandrov Vazov <nikolaiv@itf-lifeportal-core-d01.hpc.uio.no>
2017-02-22 12:16:55 +0100

    Imporved dynamic path

--- venv_config.sh
+++ venv_config.sh
@@ -5,8 +5,9 @@
 MYDIR="$(dirname "$(realpath "$0")")"
 
 # Modify $PYTHONPATH in .venv
-sudo -u galaxy -H sh -c 'echo export GALAXY_LIB=/home/galaxy/galaxy/lib >> /home/galaxy/galaxy/.venv/bin/activate'
-sudo -u galaxy -H sh -c 'echo export PYTHONPATH=\${GALAXY_LIB}:/home/galaxy/galaxy/lib/usit/python >> /home/galaxy/galaxy/.venv/bin/activate'
+sudo -u galaxy -H sh -c "echo export GALAXY_LIB=${GALAXYTREE}/lib >> ${GALAXYTREE}/.venv/bin/activate"
+sudo -u galaxy -H sh -c "echo export PYTHONPATH='$'GALAXY_LIB:${GALAXYTREE}/lib/usit/python >> ${GALAXYTREE}/.venv/bin/activate"
+
 
 read -p "Install module for project report generation (PDF)? [yN] " installpdfreport
 
