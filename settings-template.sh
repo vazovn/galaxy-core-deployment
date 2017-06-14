ad2080f20ca7bf9ff890c9c9ebadce974cce559d
Nikolay Vazov <vazovn@users.noreply.github.com>
2017-06-14 14:38:17 +0200

    Create settings-template.sh

--- settings-template.sh
+++ settings-template.sh
@@ -27,10 +27,10 @@ trap 'error "${BASH_SOURCE}" "${LINENO}"' ERR
 # ---------------- 
 
 # Galaxy version (branch)
-GALAXY_GIT_BRANCH=lifeportal_16.10
+GALAXY_GIT_BRANCH=release_17.01
 # Galaxy repository
 uiouser=
-GALAXY_GIT_REPO=https://${uiouser}@bitbucket.usit.uio.no/scm/ft/galaxy.git
+GALAXY_GIT_REPO=https://github.com/galaxyproject/galaxy.git
 
 # Galaxy user (for /etc/passwd)
 GALAXYUSER=galaxy
@@ -52,98 +52,8 @@ GALAXYDBHOST=
 # Set to SKIP for skipping change
 # ------------------------------------------
 
-# Config file names
-GALAXY_TOOL_CONF=config/tool_conf.xml.lifeportal
-GALAXY_JOB_CONF=config/job_conf.xml.lifeportal
-GALAXY_TOOL_DATA_TABLE_CONF=config/tool_data_table_conf.xml.lifeportal
-GALAXY_DATATYPES_CONF=SKIP
-
-# Tools and tools_data folder
-GALAXY_TOOL_PATH=${GALAXYUSERHOME}/tools_lifeportal
-GALAXY_TOOL_DATA_LOCAL=tool_data_lifeportal
-
-# Tools and tools_data repository (set to "SKIP", to not use this)
-GALAXY_TOOLS_REPO=https://${uiouser}@bitbucket.usit.uio.no/scm/ft/lifeportal_tool_config.git
-GALAXY_TOOL_DATA_REPO=https://${uiouser}@bitbucket.usit.uio.no/scm/ft/lifeportal_tool_data.git
-
 # Brand and public hostname
 GALAXY_BRAND=Lifeportal
-GALAXY_PUBLIC_HOSTNAME=lifeportal.uio.no
-GALAXY_ADMIN_USERS=n.a.vazov@usit.uio.no,sabry.razick@usit.uio.no,trond.thorbjornsen@usit.uio.no
-GALAXY_HELP_EMAIL=lifeportal-help@usit.uio.no
-
-# When using remote authentication, this shall be set
-GALAXY_LOGOUT_URL=https://${GALAXY_PUBLIC_HOSTNAME}/callback?logout=https://${GALAXY_PUBLIC_HOSTNAME}/logout
-
-
-# The rest of the file is only needed when abel is mounted
-# --------------------------------------------------------
-
-# Will this server have abel mounted:
-GALAXY_ABEL_MOUNT=1
-
-# Example: GALAXY_DATABASE_DIRNAME=database_galaxy_prod01
-# Must be set, when abel is mounted:
-GALAXY_DATABASE_DIRNAME=
-
-if [[ ${GALAXY_ABEL_MOUNT} != 1 ]] && [ -z ${GALAXY_DATABASE_DIRNAME} ]; then
-    echo Please fill out GALAXY_DATABASE_DIRNAME in settings.sh
-fi
-
-ABEL_WORK_PATH=/work/projects/galaxy/data
-
-# Generate variables from ABEL_WORK_PATH and GALAXY_DATABASE_DIRNAME                            
-# --------------------------------------------------------------------------------------------- #
-GALAXY_DATABASE_DIRECTORY_ON_CLUSTER=${ABEL_WORK_PATH}/${GALAXY_DATABASE_DIRNAME}               #
-GALAXY_FILEPATH=${ABEL_WORK_PATH}/${GALAXY_DATABASE_DIRNAME}/files                              #
-GALAXY_NEW_FILEPATH=${ABEL_WORK_PATH}/${GALAXY_DATABASE_DIRNAME}/tmp                            # 
-GALAXY_JOB_WORKING_DIRECTORY=${ABEL_WORK_PATH}/${GALAXY_DATABASE_DIRNAME}/job_working_directory #
-GALAXY_CLUSTER_FILES_DIRECTORY=${ABEL_WORK_PATH}/${GALAXY_DATABASE_DIRNAME}/slurm               #
-GALAXY_TOOL_DATA_PATH=${ABEL_WORK_PATH}/${GALAXY_DATABASE_DIRNAME}/${GALAXY_TOOL_DATA_LOCAL}    #
-# --------------------------------------------------------------------------------------------- #
-
-EXTERNAL_DBS_LINK_NAME=/home/galaxy/galaxy/lib/usit/external_dbs
-EXTERNAL_DBS_PATH=/work/projects/galaxy/external_dbs
-
-
-# Project admins
-# --------------
-PROJECT_ADMIN_USERS=n.a.vazov@usit.uio.no,sabry.razick@usit.uio.no,trond.thorbjornsen@usit.uio.no
-
-
-# GOLD settings
-# -------------
-
-GOLD_SRC_DIRECTORY=gold-2.2.0.5
-GOLD_INSTALLATION_DIRECTORY=/opt
-
-# Must be set, when GOLD is used:
-GOLDDB=
-GOLDDBUSER=
-GOLDDBPASSWD=
-GOLDDBHOST=
-
-# FILESENDER settings (Fill in if the "big file upload" option is enabled)
-# -------------
-
-# Must be set, when FILESENDER is used:
-FILESENDERDBNAME=
-FILESENDERUSER=
-FILESENDERPASSWORD=
-FILESENDERHOST=
-
-FILESENDER_ADMINISTRATOR=
-FILESENDER_ADMIN_EMAIL=
-
-ABEL_FILESENDER_PATH=/work/projects/galaxy/filesender
-FILESENDER_STORAGE=${ABEL_FILESENDER_PATH}/${GALAXY_PUBLIC_HOSTNAME}
-FILESENDER_URL=filesender.${GALAXY_PUBLIC_HOSTNAME}
-SIMPLESAMLPHP_VERSION=
-ABEL_SIMPLESAML_PATH=/work/projects/galaxy/simplesaml
-FILESENDER_SIMPLESAML=${ABEL_SIMPLESAML_PATH}/${GALAXY_PUBLIC_HOSTNAME}
-
-
-## Certificates must be provided for the domain FILESENDER_URL
-## Could be a multi-domain certificate (galaxy + filesender) or only for filesender's virt host
-FILESENDER_SSL_CERTIFICATE_PATH=
-FILESENDER_SSL_KEYFILE_PATH=
+GALAXY_PUBLIC_HOSTNAME=
+GALAXY_ADMIN_USERS=
+GALAXY_HELP_EMAIL=
