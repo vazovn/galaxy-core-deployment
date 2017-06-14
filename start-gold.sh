0927123cba996b257d247a0124de6ce20701a978
Nikolay Vazov <nikolaiv@1x-193-157-241-161.uio.no>
2017-06-14 13:00:22 +0200

    Clean repo for GCC2017

--- start-gold.sh
+++
@@ -1,6 +0,0 @@
-if [ ${USER} != "gold" ]; then
-    echo "Gold should not run as ${USER}"
-    exit 1
-fi
-
-/opt/gold/sbin/goldd --start
--- stop-gold.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ ${USER} != "gold" && ${USER} != "root" ]]; then
-    echo "Gold can not be stopped from ${USER}"
-    exit 1
-fi
-
-/opt/gold/sbin/goldd --stop
